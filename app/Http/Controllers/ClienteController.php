cfa5bc1697e5bfb44e0d4bcb8834fa0259fe9dd8
ArmstrongKN <kioshinambu@hotmail.com>
2024-02-16 23:03:59 -0300

    front

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    //
+    public function showhome(){
+        return view("home");
+    }
+    public function showFormularioCadastro(Request $request){
+        return view("formularioCadastroCliente");
+    }
 }
